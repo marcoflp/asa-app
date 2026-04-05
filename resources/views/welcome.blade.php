08405c67531d67109f5de5d76a90a0b5e2db5830
marcoflp <marco.felipi@hotmail.com>
2026-04-05 14:27:57 -0300

    UI: Fix favicon on unauthenticated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,8 @@
 
         <title>ASA - Ação Solidária Adventista</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/logo.png" type="image/png">
+        <link rel="apple-touch-icon" href="/logo.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
